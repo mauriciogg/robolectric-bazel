e0c7436e209d6905733b13a4f10fe43bfa60d90a
Mauricio Galindo <up.mauricio.g@gmail.com>
2021-10-06 09:50:36 -0700

    Update gen-deps.py

--- bazel/gen-deps.py
+++ bazel/gen-deps.py
@@ -4,7 +4,7 @@ def getKey(jar):
     group_and_artifact = "org.robolectric\:android-all-instrumented\:"
     # Given "external/org_robolectric_android_all_4_1_2_r1_robolectric_r1/jar/android-all-4.1.2_r1-robolectric-r1.jar"
     # We want "4.1.2_r1-robolectric-r1.jar"
-    version = jar.split("android-all-instrumented")[1][:-4]
+    version = jar.split("android-all-instrumented-")[1][:-4]
     return group_and_artifact + version
 
 def getValue(jar):
